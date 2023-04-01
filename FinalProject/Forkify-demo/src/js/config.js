9ef4f871c5eb5c8140324de9f4cb96903fa581fe
Krittin Saenpakdi <gump29062@gmail.com>
2023-04-01 23:06:51 +0700

    Add Forkify-demo

---
+++ FinalProject/Forkify-demo/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6e866b54-afe7-4313-817e-8c8a3e5d327d';
+export const MODAL_CLOSE_SEC = 2.5;
